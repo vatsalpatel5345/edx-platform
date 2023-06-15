e9422f59b52e2b2f27386e29c41bacc1e3847533
Shahbaz Shabbir <shbzshbr@gmail.com>
2023-06-15 06:21:08 +0500

    feat: add a command to fetch unsubscribed emails from Braze

---
+++ common/djangoapps/student/management/commands/retrieve_unsubscribed_emails.py
@@ -0,0 +1,69 @@
+import csv
+import logging
+import uuid
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand, CommandError
+
+from lms.djangoapps.utils import get_braze_client
+
+logger = logging.getLogger(__name__)
+
+UNSUBSCRIBED_EMAILS_MAX_LIMIT = 500
+
+
+class Command(BaseCommand):
+    help = """
+    Retrieve unsubscribed emails from Braze API based on specified parameters.
+
+    Usage:
+    python manage.py retrieve_unsubscribed_emails [--start-date START_DATE] [--end-date END_DATE]
+
+    Options:
+      --start-date START_DATE   Start date (optional)
+      --end-date END_DATE       End date (optional)
+
+    Example:
+        $ ... retrieve_unsubscribed_emails --start-date 2022-01-01 --end-date 2023-01-01
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('--start-date', dest='start_date', help='Start date')
+        parser.add_argument('--end-date', dest='end_date', help='End date')
+
+    def handle(self, *args, **options):
+        emails = []
+        start_date = options.get('start_date')
+        end_date = options.get('end_date')
+
+        if not start_date and not end_date:
+            start_date = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+            end_date = datetime.now().strftime('%Y-%m-%d')
+
+        try:
+            braze_client = get_braze_client()
+            if braze_client:
+                emails = braze_client.retrieve_unsubscribed_emails(
+                    start_date=start_date,
+                    end_date=end_date,
+                    limit=UNSUBSCRIBED_EMAILS_MAX_LIMIT
+                )
+                self.stdout.write(self.style.SUCCESS('Unsubscribed emails retrieved successfully'))
+        except Exception as exc:  # pylint: disable=broad-except
+            logger.exception(f'Unable to retrieve unsubscribed emails from Braze due to : {exc}')
+            raise CommandError(f'Unable to retrieve unsubscribed emails from Braze due to : {exc}')
+
+        output_filename = f'{str(uuid.uuid4())}.csv'
+
+        try:
+            with open(output_filename, 'w', newline='') as output_file:
+                csv_writer = csv.writer(output_file)
+                csv_writer.writerow(('email', 'unsubscribed_at'))
+                rows = [(email['email'], email['unsubscribed_at']) for email in emails]
+                csv_writer.writerows(rows)
+                self.stdout.write(
+                    self.style.SUCCESS(f'Unsubscribed emails write in CSV file {output_filename} successfully'))
+        except OSError:
+            logger.exception(f'Error writing to file: {output_filename}')
+            raise CommandError(
+                f'Error writing to file: {output_filename}')  # lint-amnesty, pylint: disable=raise-missing-from
---
+++ common/djangoapps/student/management/tests/test_retrieve_unsubscribed_emails.py
@@ -0,0 +1,118 @@
+import os
+from io import StringIO
+from tempfile import NamedTemporaryFile
+from unittest.mock import patch
+
+import six
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase
+
+
+class RetrieveUnsubscribedEmailsTests(TestCase):
+    """
+    Tests for the retrieve_unsubscribed_emails command.
+    """
+
+    def setUp(self):
+        """
+        Set up tests
+        """
+        super().setUp()
+
+    @staticmethod
+    def _write_test_csv(csv, lines):
+        """
+        Write a test csv file with the lines provided
+        """
+        csv.write(b"email,unsubscribed_at\n")
+        for line in lines:
+            csv.write(six.b(line))
+        csv.seek(0)
+        return csv
+
+    @patch('common.djangoapps.student.management.commands.retrieve_unsubscribed_emails.get_braze_client')
+    def test_retrieve_unsubscribed_emails_command(self, mock_get_braze_client):
+        """
+        Test the retrieve_unsubscribed_emails command
+        """
+        mock_braze_client = mock_get_braze_client.return_value
+        mock_braze_client.retrieve_unsubscribed_emails.return_value = [
+            {'email': 'test1@example.com', 'unsubscribed_at': '2023-06-01 10:00:00'},
+            {'email': 'test2@example.com', 'unsubscribed_at': '2023-06-02 12:00:00'},
+        ]
+
+        output = StringIO()
+        call_command('retrieve_unsubscribed_emails', stdout=output)
+
+        self.assertIn('Unsubscribed emails retrieved successfully', output.getvalue())
+        print("\n\n\nOUTPUT ", output.getvalue(), "\n\n\n")
+
+        with NamedTemporaryFile(delete=False) as csv:
+            filepath = csv.name
+            lines = [
+                'test1@example.com,2023-06-01 10:00:00',
+                'test2@example.com,2023-06-02 12:00:00'
+            ]
+            self._write_test_csv(csv, lines)
+
+            with open(filepath, 'r') as csv_file:
+                csv_data = csv_file.read()
+                self.assertIn('test1@example.com,2023-06-01 10:00:00', csv_data)
+                self.assertIn('test2@example.com,2023-06-02 12:00:00', csv_data)
+
+            os.remove(filepath)
+
+    @patch('common.djangoapps.student.management.commands.retrieve_unsubscribed_emails.get_braze_client')
+    def test_retrieve_unsubscribed_emails_command_with_dates(self, mock_get_braze_client):
+        """
+        Test the retrieve_unsubscribed_emails command with custom start and end dates.
+        """
+        mock_braze_client = mock_get_braze_client.return_value
+        mock_braze_client.retrieve_unsubscribed_emails.return_value = [
+            {'email': 'test3@example.com', 'unsubscribed_at': '2023-06-03 08:00:00'},
+            {'email': 'test4@example.com', 'unsubscribed_at': '2023-06-04 14:00:00'},
+        ]
+
+        output = StringIO()
+        call_command(
+            'retrieve_unsubscribed_emails',
+            '--start-date', '2023-06-03',
+            '--end-date', '2023-06-04',
+            stdout=output
+        )
+        print("\n\n\nOUTPUT ", output.getvalue(), "\n\n\n")
+
+        self.assertIn('Unsubscribed emails retrieved successfully', output.getvalue())
+
+        with NamedTemporaryFile(delete=False) as csv:
+            filepath = csv.name
+            lines = [
+                'test3@example.com,2023-06-03 08:00:00',
+                'test4@example.com,2023-06-04 14:00:00'
+            ]
+            self._write_test_csv(csv, lines)
+
+            with open(filepath, 'r') as csv_file:
+                csv_data = csv_file.read()
+                self.assertIn('test3@example.com,2023-06-03 08:00:00', csv_data)
+                self.assertIn('test4@example.com,2023-06-04 14:00:00', csv_data)
+
+            os.remove(filepath)
+
+    @patch('common.djangoapps.student.management.commands.retrieve_unsubscribed_emails.get_braze_client')
+    @patch('common.djangoapps.student.management.commands.retrieve_unsubscribed_emails.logger.exception')
+    def test_retrieve_unsubscribed_emails_command_exception(self, mock_logger_exception, mock_get_braze_client):
+        """
+        Test the retrieve_unsubscribed_emails command when an exception is raised.
+        """
+        mock_braze_client = mock_get_braze_client.return_value
+        mock_braze_client.retrieve_unsubscribed_emails.side_effect = Exception('Braze API error')
+
+        output = StringIO()
+        with self.assertRaises(CommandError):
+            call_command('retrieve_unsubscribed_emails', stdout=output)
+
+        mock_logger_exception.assert_called_once_with(
+            'Unable to retrieve unsubscribed emails from Braze due to : Braze API error'
+        )
